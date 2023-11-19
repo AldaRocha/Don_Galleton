caf966c040a2c6c9706487347f1dafb61611663b
Alda <rochaaldanafcojavier@gmail.com>
2023-11-18 21:53:27 -0600

    Proyecto iniciado

---
+++ DonGalleto/web/js/loginController.js
@@ -0,0 +1,11 @@
+
+/*
+    Metodos generales que se pueden usar en todo el proyecto
+    Como convertir de kg a oz, litros a kilos por ejemplo
+
+    Por defecto debe ir:
+    °   Login
+    °   Logout
+    °   Santitizar
+    °   Normalizar
+ */
---
+++ DonGalleto/web/modules/main/mainController.js
@@ -0,0 +1,5 @@
+export function inicializar(){
+    
+}
+
+// Aqui van las funciones que mandan a llamar a las vistas solamente
