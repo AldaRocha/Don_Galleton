958a9d8545dfa9aa88a1d45cfeeacd9ccec2dfc5
JorgeeMorales <jorge.morales241103@gmail.com>
2023-11-19 23:30:21 -0600

    Se agrego fetch de moduloProducto.

--- DonGalleto/web/modules/main/mainController.js
+++ DonGalleto/web/modules/main/mainController.js
@@ -1,5 +1,29 @@
-export function inicializar(){
-    
+//export function inicializar(){
+//    
+//}
+
+let moduloProducto;
+
+
+function cargarModuloProductos() {
+    fetch("../../modules/producto/vista_producto.html")
+            .then(
+                    function (response) {
+                        console.log(response);
+                        return response.text();
+                    }
+            )
+            .then(
+                    function (html) {
+                        document.getElementById("contenedorPrincipal").innerHTML = html;
+                        import("../producto//productoController.js").then(
+                                function (controller) {
+                                    moduloProducto = controller;
+                                    moduloProducto.inicializar();
+                                }
+                        );
+                    }
+            );
 }
 
 // Aqui van las funciones que mandan a llamar a las vistas solamente
